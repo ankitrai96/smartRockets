ac2846efd1a0127ddceb28382570ec8e0af8de87
Ankit Rai <raiankit953@gmail.com>
2017-08-27 14:59:31 +0530

    multiple rocktes in the air

--- sketch.js
+++ sketch.js
@@ -1,19 +1,51 @@
-var rocket;
+var population,
+  count=0,
+  lifeSpan=200,
+  life, target;
 function setup() {
 
 	createCanvas(600,400);
-  rocket = new Rocket();
+  population = new Population();
+  life = createP();
+  target = createVector(width/2, 50);
 }
 
 function draw() {
-  background(55,55,45);
-  rocket.show();
+  background(0);
+  population.run();
+  life.html(count);
+  count++;
+  fill(0,255,13);
+  ellipse(target.x, target.y, 25,25);
+}
+
+function DNA(){
+  this.genes = [];
+  for(var i = 0; i < lifeSpan; i++){
+    this.genes[i] = p5.Vector.random2D();
+    this.genes[i].setMag(0.1);
+  }
+}
+
+function Population(){
+  this.rockets = [];
+  this.popSize = 25;
+  for(var i = 0; i<this.popSize; i++){
+    this.rockets[i] = new Rocket();
+  }
+  this.run = function(){
+    for(var i = 0; i<this.popSize; i++){
+      this.rockets[i].update();
+      this.rockets[i].show();
+    }  
+  }
 }
 
 function Rocket(){
   this.vel = createVector();
   this.pos = createVector(width/2,height);
   this.acc = createVector();
+  this.dna = new DNA();
   
   this.applyForce = function(force){
     this.acc.add(force);
@@ -24,12 +56,16 @@ function Rocket(){
     translate(this.pos.x, this.pos.y);
     rotate(this.vel.heading());
     rectMode(CENTER);
-    rect(0,0,5,50);
+    fill(235,2,2);
+    noStroke();
+    rect(0,0,25,5);
     pop();
   }
   this.update = function(){
+    this.applyForce(this.dna.genes[count]);
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.mult(0);
+    
   }
 }

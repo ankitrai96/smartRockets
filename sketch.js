f8f5865875a227093c07e6cf26a4edf07d3a1b60
Ankit Rai <raiankit953@gmail.com>
2017-08-27 14:06:59 +0530

    single rocket comes

--- sketch.js
+++ sketch.js
@@ -1,9 +1,35 @@
+var rocket;
 function setup() {
 
-	createCanvas(400,600);
-
+	createCanvas(600,400);
+  rocket = new Rocket();
 }
 
 function draw() {
   background(55,55,45);
+  rocket.show();
+}
+
+function Rocket(){
+  this.vel = createVector();
+  this.pos = createVector(width/2,height);
+  this.acc = createVector();
+  
+  this.applyForce = function(force){
+    this.acc.add(force);
+  }
+
+  this.show = function(){
+    push();
+    translate(this.pos.x, this.pos.y);
+    rotate(this.vel.heading());
+    rectMode(CENTER);
+    rect(0,0,5,50);
+    pop();
+  }
+  this.update = function(){
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
 }
